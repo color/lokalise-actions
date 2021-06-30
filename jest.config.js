ceceeb88825411604d479fa6aeabe8e57e39d76d
Chris Chow <chris.chow@color.com>
2021-06-29 17:34:47 -0700

    Customize default settings
    - Use tsconfig paths
    - Change linting rules

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  moduleNameMapper: {
+    "@src/(.*)": "<rootDir>/src/$1",
+  },
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
